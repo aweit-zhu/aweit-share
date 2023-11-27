3f446ae987006d9a462e680e9346c3ae3919059f
阿偉特 <aa4192696@gmail.com>
2023-11-27 11:32:14 +0800

    update

--- Java 相關/LayoutDemo/src/main/java/com/example/controller/DownloadServlet.java
+++ Java 相關/LayoutDemo/src/main/java/com/example/controller/DownloadServlet.java
@@ -4,7 +4,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,16 +27,14 @@ public class DownloadServlet extends HttpServlet {
 		resp.setContentType("APPLICATION/OCTET-STREAM");
 		resp.setHeader("Content-Disposition", "attachment; filename=\"" + filenameEncode(filename) + "\"");
 
-		try (InputStream input = getClass().getClassLoader().getResourceAsStream(filename);
-			 OutputStream output = resp.getOutputStream()) {
-			byte[] buffer = new byte[4096];
-		    int bytesRead;
-		    while ((bytesRead = input.read(buffer)) != -1) {
-		        output.write(buffer, 0, bytesRead);
-		    }
-		} catch (Exception e) {
-			throw e;
-		}
+		try (OutputStream output = resp.getOutputStream()){
+			URL resource = getClass().getClassLoader().getResource(filename);
+			Path sorucrePath = Paths.get(resource.toURI());
+			Files.copy(sorucrePath, output);
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} 
+
 	}
 
 	public static String filenameEncode(String name) {
@@ -42,3 +46,16 @@ public class DownloadServlet extends HttpServlet {
 	    }
 	}
 }
+
+
+// 傳統方法：
+//try (InputStream input = getClass().getClassLoader().getResourceAsStream(filename);
+//OutputStream output = resp.getOutputStream()) {
+//byte[] buffer = new byte[4096];
+//int bytesRead;
+//while ((bytesRead = input.read(buffer)) != -1) {
+//   output.write(buffer, 0, bytesRead);
+//}
+//} catch (Exception e) {
+//throw e;
+//}
