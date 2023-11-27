3ea464b0f71eb747681ba4232de9827ce033c6fd
aweit-zhu <aa4192696@gmail.com>
2023-11-27 21:41:06 +0800

    QR Code update

--- Java 相關/LayoutDemo/src/test/java/BCryptExample.java
+++ Java 相關/LayoutDemo/src/test/java/BCryptExample.java
@@ -3,13 +3,13 @@ import org.mindrot.jbcrypt.BCrypt;
 public class BCryptExample {
 
     public static void main(String[] args) {
-        // 加密密码
+
         String rawPassword = "123";
         String hashedPassword = BCrypt.hashpw(rawPassword, BCrypt.gensalt());
         System.out.println("Hashed Password: " + hashedPassword); // $2a$10$ms1UGigcUkZP0axeEY4JM.3bG9CawnqegKEH2Dlw.fJAv.wLF6Zf.
 
-        // 验证密码
-        String candidatePassword = "123"; // 用户输入的密码
+
+        String candidatePassword = "123";
         boolean isPasswordMatch = BCrypt.checkpw(candidatePassword, hashedPassword);
         System.out.println("Password Match: " + isPasswordMatch);
     }
