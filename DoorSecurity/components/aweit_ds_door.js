a443d95b1fba47d63b7ceca4e70806e886815055
aweit-zhu <aa4192696@gmail.com>
2023-12-02 01:05:44 +0800

    update

--- DoorSecurity/components/aweit_ds_door.js
+++ DoorSecurity/components/aweit_ds_door.js
@@ -146,7 +146,8 @@ class DoorSecurity extends HTMLElement {
         if (name === 'status' && value == 1) {
             root.innerHTML = doorOpen.replace("doorName", this.getAttribute('door_name'))
         } else if(name === 'status' && value == 0) {
-            root.innerHTML = doorClose.replace("doorName", this.getAttribute('door_name'))
+        //    root.innerHTML = doorClose.replace("doorName", this.getAttribute('door_name'))
+            root.innerHTML = doorStop.replace("doorName", this.getAttribute('door_name'))
         } else if(name === 'status' && value == -1) {
             root.innerHTML = doorStop.replace("doorName", this.getAttribute('door_name'))
         }
