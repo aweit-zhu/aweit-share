608e27d5bf2d123d0f1077d28b9250cc7bfdc7c0
aweit-zhu <aa4192696@gmail.com>
2023-12-13 20:24:37 +0800

    update

--- Java 相關/LayoutSpringMVC/src/main/java/com/aweit/controller/HelloController.java
+++ Java 相關/LayoutSpringMVC/src/main/java/com/aweit/controller/HelloController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.aweit.bean.Book;
+import com.aweit.bean.Emp;
 
 @Controller
 @RequestMapping("/hello")
@@ -14,5 +18,18 @@ public class HelloController {
 		model.addAttribute("message", "Hello Spring MVC Framework!");
 		return "hello";
 	}
+	
+	@GetMapping(value = "/one")
+	@ResponseBody
+	public String one(String str) {
+		System.out.println(str);
+		return "hello";
+	}
 
+	@GetMapping(value = "/two")
+	@ResponseBody
+	public String one(Emp emp,Emp emp2,Book book) {
+		System.out.println(emp);
+		return "hello";
+	}
 }
