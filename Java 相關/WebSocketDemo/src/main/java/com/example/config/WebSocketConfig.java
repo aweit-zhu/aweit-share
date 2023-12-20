bb6e0e6223e0d8e964c46eb112fa1ce5157100d5
aweit-zhu <aa4192696@gmail.com>
2023-12-20 23:06:33 +0800

    update

--- Java 相關/WebSocketDemo/src/main/java/com/example/config/WebSocketConfig.java
+++ Java 相關/WebSocketDemo/src/main/java/com/example/config/WebSocketConfig.java
@@ -1,10 +1,8 @@
 package com.example.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
--- Java 相關/WebSocketDemo/src/main/java/com/example/handler/ServerWebSocketHandler.java
+++ Java 相關/WebSocketDemo/src/main/java/com/example/handler/ServerWebSocketHandler.java
@@ -8,7 +8,6 @@ import java.util.concurrent.CopyOnWriteArraySet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
