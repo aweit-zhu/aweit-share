0bef230df3896a728ea7a2fb71df13ee88e8f5a2
阿偉特 <aa4192696@gmail.com>
2023-12-22 10:13:32 +0800

    update

--- Java 相關/yahoo-finance-api/src/main/java/org/cdri/YahooFinance.java
+++ Java 相關/yahoo-finance-api/src/main/java/org/cdri/YahooFinance.java
@@ -23,150 +23,150 @@ import com.google.gson.Gson;
 
 public class YahooFinance {
 
+	private final static String STOCK_CURRENT_API_URL = "https://query1.finance.yahoo.com/v7/finance/download/%s";
+
+	private final static String STOCK_HISTORY_API_URL = "https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%s&period2=%s&interval=1d&events=history";
+
 	private final static String blueColor = "\033[1;34m";
 
 	private final static String resetColor = "\033[0m";
 
 	public static void main(String[] args) throws ParseException, ClientProtocolException, IOException {
-		
-		System.setProperty("stock.api.url",
+
+		System.setProperty("stock.current.api.url", "https://query1.finance.yahoo.com/v7/finance/download/%s");
+
+		System.setProperty("stock.history.api.url",
 				"https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%s&period2=%s&interval=1d&events=history");
+
 		try {
+			System.out.println(YahooFinance.getLatestStockData("0050.TW")); // 今日交易紀錄
 			System.out.println(YahooFinance.getLatestStockData("0050.TW", "1D")); // 最近一日的每日交易紀錄
-//			System.out.println(YahooFinance.getLatestStockData("0050.TW", "1W")); // 最近一周的每日交易紀錄
+			System.out.println(YahooFinance.getLatestStockData("0050.TW", "1W")); // 最近一周的每日交易紀錄
 //			System.out.println(YahooFinance.getLatestStockData("0050.TW", "1M")); // 最近一月的每日交易紀錄
 //			System.out.println(YahooFinance.getLatestStockData("0050.TW", "2S")); // 最近一季的每日交易紀錄
 //			System.out.println(YahooFinance.getLatestStockData("0050.TW", "1Y")); // 最近一年的每日交易紀錄
 
-//			System.out.println(YahooFinance.getLatestStockData("0050.TW", TimeUnit.D)); // 最近一日的每日交易紀錄
-			System.out.println(YahooFinance.getLatestStockData("0050.TW", TimeUnit.W)); // 最近一周的每日交易紀錄
-//			System.out.println(YahooFinance.getLatestStockData("0050.TW", TimeUnit.M)); // 最近一月的每日交易紀錄
-//			System.out.println(YahooFinance.getLatestStockData("0050.TW", TimeUnit.S)); // 最近一季的每日交易紀錄
-//			System.out.println(YahooFinance.getLatestStockData("0050.TW", TimeUnit.Y)); // 最近一年的每日交易紀錄
-
-//			System.out.println(YahooFinance.getLatestStockData("0050.TW", TimeUnit.D, 2)); // 最近二日的每日交易紀錄
-//			System.out.println(YahooFinance.getLatestStockData("0050.TW", TimeUnit.W, 2)); // 最近二周的每日交易紀錄
-//			System.out.println(YahooFinance.getLatestStockData("0050.TW", TimeUnit.M, 2)); // 最近二月的每日交易紀錄
-			System.out.println(YahooFinance.getLatestStockData("0050.TW", TimeUnit.S, 2)); // 最近二季的每日交易紀錄
-//			System.out.println(YahooFinance.getLatestStockData("0050.TW", TimeUnit.Y, 2)); // 最近二年的每日交易紀錄
 		} catch (IOException | RangeFormatException e) {
 			e.printStackTrace();
-		} 
+		}
 	}
-	
-	private static void validateRange(String range) throws RangeFormatException{
-		
-		if (range == null) throw new RangeFormatException(String.format("range is null", null));
-		
-		if (range.length() < 2) throw new RangeFormatException(String.format("range is invalid.", null));
-		
+
+	private static void validateRange(String range) throws RangeFormatException {
+
+		if (range == null)
+			throw new RangeFormatException(String.format("range is null", null));
+
+		if (range.length() < 2)
+			throw new RangeFormatException(String.format("range is invalid.", null));
+
 		// 單位檢查
 		String unitString = range.substring(range.length() - 1).toUpperCase();
 		TimeUnit[] units = TimeUnit.values();
-		boolean flag =  Arrays.asList(units).stream().anyMatch(unit-> unit.name().equals(unitString));
-		if(!flag){
+		boolean flag = Arrays.asList(units).stream().anyMatch(unit -> unit.name().equals(unitString));
+		if (!flag) {
 			throw new RangeFormatException("range is invalid. The valid unit is in [Y,W,M,S]");
 		}
-		
+
 		// 數值檢查
-		String numberString =  range.substring(0,range.length() - 1).toUpperCase();
+		String numberString = range.substring(0, range.length() - 1).toUpperCase();
 		try {
 			Integer.parseInt(numberString);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			throw new RangeFormatException(String.format("range is invalid. %s is not number", numberString));
 		}
-		
-		TimeUnit unit = Arrays.asList(units).stream().filter(u-> u.name().equals(unitString)).findAny().orElse(null);
+
+		TimeUnit unit = Arrays.asList(units).stream().filter(u -> u.name().equals(unitString)).findAny().orElse(null);
 		int number = Integer.parseInt(numberString);
 
-		if(number < 1) {
+		if (number < 1) {
 			throw new RangeFormatException(String.format("range is invalid. %s is is not more than 1 ", numberString));
-		} 
+		}
 
 	}
-	
+
 	/**
 	 * 
 	 * @param stockNo: 股票號碼
-	 * @param range: 時間區間。例如：1D、1W、1M、1S、1Y
+	 * @param range:   時間區間。例如：1D、1W、1M、1S、1Y
 	 * @return
 	 * @throws IOException
 	 * @throws RangeFormatException
 	 */
 	public static String getLatestStockData(String stockNo, String range) throws IOException, RangeFormatException {
-		
+
 		validateRange(range);
-		
+
 		String unitString = range.substring(range.length() - 1).toUpperCase();
-		String numberString =  range.substring(0,range.length() - 1).toUpperCase();
-		
+		String numberString = range.substring(0, range.length() - 1).toUpperCase();
+
 		TimeUnit[] units = TimeUnit.values();
-		TimeUnit unit = Arrays.asList(units).stream().filter(u-> u.name().equals(unitString)).findAny().get();
+		TimeUnit unit = Arrays.asList(units).stream().filter(u -> u.name().equals(unitString)).findAny().get();
 		int number = Integer.parseInt(numberString);
 
-		return getLatestStockData(stockNo,unit,number);
+		return getLatestStockData(stockNo, unit, number);
 	}
-	
+
 	/**
 	 * 
 	 * @param stockNo: 股票號碼
-	 * @param unit: TimeUnit 列舉
+	 * @param unit:    TimeUnit 列舉
 	 * @return
 	 * @throws IOException
 	 * @throws RangeFormatException
 	 */
 	public static String getLatestStockData(String stockNo, TimeUnit unit) throws IOException, RangeFormatException {
-		return getLatestStockData(stockNo,unit,1);
+		return getLatestStockData(stockNo, unit, 1);
 	}
 
 	/**
 	 * 
 	 * @param stockNo: 股票號碼
-	 * @param unit: TimeUnit 列舉
-	 * @param number: 區間範圍
+	 * @param unit:    TimeUnit 列舉
+	 * @param number:  區間範圍
 	 * @return
 	 * @throws IOException
 	 * @throws RangeFormatException
 	 */
-	public static String getLatestStockData(String stockNo, TimeUnit unit, int number) throws IOException, RangeFormatException {
-		
-		validateRange(String.valueOf(number)+unit.name());
-		
+	public static String getLatestStockData(String stockNo, TimeUnit unit, int number)
+			throws IOException, RangeFormatException {
+
+		validateRange(String.valueOf(number) + unit.name());
+
 		LocalDate endDate = LocalDate.now();
 		LocalDate startDate = null;
-		
-		if(unit.equals(TimeUnit.Y)) {
+
+		if (unit.equals(TimeUnit.Y)) {
 			startDate = endDate.minusYears(number);
-		} else if(unit.equals(TimeUnit.W)) {
+		} else if (unit.equals(TimeUnit.W)) {
 			startDate = endDate.minusWeeks(number);
-		} else if(unit.equals(TimeUnit.M)) {
+		} else if (unit.equals(TimeUnit.M)) {
 			startDate = endDate.minusMonths(number);
-		} else if(unit.equals(TimeUnit.D)) {
+		} else if (unit.equals(TimeUnit.D)) {
 			startDate = endDate.minusDays(number);
-		} else if(unit.equals(TimeUnit.S)) {
+		} else if (unit.equals(TimeUnit.S)) {
 			int month = endDate.getMonthValue(); // 1-12
 			Season season = getLatestSeason(month);
 			startDate = LocalDate.of(endDate.getYear(), season.getStartMonth(), 1);
 			startDate = startDate.minusMonths((number - 1) * 3);
 		}
-		
-		return getStockData(stockNo,startDate,endDate);
+
+		return getStockData(stockNo, startDate, endDate);
 	}
-	
+
 	private static Season getLatestSeason(int month) {
-        if (month >= 1 && month <= 3) {
-            return Season.FIRST;
-        } else if (month >= 4 && month <= 6) {
-            return Season.SECOND;
-        } else if (month >= 7 && month <= 9) {
-            return Season.THRID;
-        } else if (month >= 10 && month <= 12) {
-            return Season.FOURTH;
-        } else {
-            return null;
-        }
-    }
-	
+		if (month >= 1 && month <= 3) {
+			return Season.FIRST;
+		} else if (month >= 4 && month <= 6) {
+			return Season.SECOND;
+		} else if (month >= 7 && month <= 9) {
+			return Season.THRID;
+		} else if (month >= 10 && month <= 12) {
+			return Season.FOURTH;
+		} else {
+			return null;
+		}
+	}
+
 	/**
 	 * 
 	 * @param stockNo: 股票號碼
@@ -174,16 +174,14 @@ public class YahooFinance {
 	 * @throws IOException
 	 */
 	public static String getLatestStockData(String stockNo) throws IOException {
-		LocalDate endDate = LocalDate.now();
-		LocalDate startDate = endDate.minusDays(1);
-		return getStockData(stockNo, startDate, endDate);
+		return getStockData(stockNo);
 	}
 
 	/**
 	 * 
-	 * @param stockNo:   股票號碼:0050.TW
+	 * @param stockNo: 股票號碼:0050.TW
 	 * @param startDate: 起始日期:2023-12-01
-	 * @param endDate:   結束日期:2023-12-31
+	 * @param endDate: 結束日期:2023-12-31
 	 * @return
 	 * @throws IOException
 	 */
@@ -195,9 +193,9 @@ public class YahooFinance {
 
 	/**
 	 * 
-	 * @param stockNo:   股票號碼
+	 * @param stockNo: 股票號碼
 	 * @param startDate: 起始日期
-	 * @param endDate:   結束日期
+	 * @param endDate: 結束日期
 	 * @return
 	 * @throws IOException
 	 */
@@ -205,9 +203,33 @@ public class YahooFinance {
 		// 將開始和結束日期轉換為 Unix 時間戳（秒為單位）
 		long period1 = startDate.atStartOfDay(ZoneId.of("UTC")).toEpochSecond();
 		long period2 = endDate.atStartOfDay(ZoneId.of("UTC")).toEpochSecond();
-		String url = String.format(System.getProperty("stock.api.url") != null ? System.getProperty("stock.api.url")
-				: "https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%s&period2=%s&interval=1d&events=history",
+		String url = String.format(
+				System.getProperty("stock.history.api.url") != null ? System.getProperty("stock.history.api.url")
+						: STOCK_HISTORY_API_URL,
 				stockNo, period1, period2);
+		System.out.printf("%s%n", blueColor + url + resetColor);
+		try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+			HttpGet request = new HttpGet(url);
+			String csvData = EntityUtils.toString(httpClient.execute(request).getEntity());
+			// �N CSV �ഫ�� JSON
+			String json = convertCsvToJson(csvData);
+			System.out.printf("%s%n", blueColor + json + resetColor);
+			return json;
+		}
+	}
+
+	/**
+	 * 
+	 * @param stockNo: 股票號碼
+	 * @return
+	 * @throws IOException
+	 */
+	public static String getStockData(String stockNo) throws IOException {
+		// 將開始和結束日期轉換為 Unix 時間戳（秒為單位）
+		String url = String.format(
+				System.getProperty("stock.current.api.url") != null ? System.getProperty("stock.current.api.url")
+						: STOCK_CURRENT_API_URL,
+				stockNo);
 		System.out.printf("%s%n", blueColor + url + resetColor);
 		try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
 			HttpGet request = new HttpGet(url);
