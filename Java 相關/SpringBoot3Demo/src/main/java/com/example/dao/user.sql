5b47aaa6d60b64657d19e4185fd40e0fa367287c
aweit-zhu <aa4192696@gmail.com>
2024-01-18 22:21:14 +0800

    update

--- Java 相關/SpringBoot3Demo/src/main/java/com/example/dao/user.sql
+++ Java 相關/SpringBoot3Demo/src/main/java/com/example/dao/user.sql
@@ -1,7 +1,8 @@
 create table users(
 	username varchar(50) not null primary key,
 	password varchar(500) not null,
-	enabled boolean not null
+	enabled boolean not null,
+	UNIQUE KEY `username_UNIQUE` (`username`)
 );
 
 create table authorities (
