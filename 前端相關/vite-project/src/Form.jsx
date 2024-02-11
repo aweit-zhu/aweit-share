b9e859cbbc1704abae5d8e59865ac4bbfdca0947
aweit-zhu <aa4192696@gmail.com>
2024-02-11 18:32:39 +0800

    update

--- 前端相關/vite-project/src/Form.jsx
+++ 前端相關/vite-project/src/Form.jsx
@@ -1,6 +1,15 @@
 import { useImmer } from 'use-immer';
+import PropTypes from 'prop-types';
+
+const initialList = [
+    { id: 0, title: 'Big Bellies', seen: false },
+    { id: 1, title: 'Lunar Landscape', seen: false },
+    { id: 2, title: 'Terracotta Army', seen: true },
+];
 
 export default function Form() {
+
+    const [list, updateList] = useImmer(initialList);
 
     const [person, updatePerson] = useImmer({
         name: 'Niki de Saint Phalle',
@@ -12,20 +21,33 @@ export default function Form() {
     });
 
     function handleChangeName(event) {
-        updatePerson(p=> {
-            p.name = event.target.value;
+        updatePerson(draft=> {
+            draft.name = event.target.value;
         });
     }
 
     function handleChangeCity(event) {
-        updatePerson(p=> {
-            p.artwork.city = event.target.value;
+        updatePerson(draft=> {
+            draft.artwork.city = event.target.value;
         });
     }
 
     function handleChangeTitle(event) {
-        updatePerson(p=> {
-            p.artwork.title = event.target.value;
+        updatePerson(draft=> {
+            draft.artwork.title = event.target.value;
+        });
+    }
+
+    function submit() {
+        console.log(person,list);
+    }
+
+    function handleToggle(artworkId, nextSeen) {
+        updateList(draft => {
+          const artwork = draft.find(a =>
+            a.id === artworkId
+          );
+          artwork.seen = nextSeen;
         });
     }
 
@@ -44,6 +66,9 @@ export default function Form() {
                     <div className='w-20 text-end self-center mr-2'>City: </div>
                     <input type="text" className='input' placeholder='城市' value={person.artwork.city} onChange={handleChangeCity}/>
                 </div>
+                <div className='flex'>
+                    <button className='btn-primary' onClick={submit}>提交</button>
+                </div>
             </div>
             <hr className='border-2 border-green-500 w-full my-3'></hr>
             <div className='flex mt-2'>
@@ -54,6 +79,37 @@ export default function Form() {
                     <p>City: {person.artwork.city}</p>
                 </div>
             </div>
+            <hr className='border-2 border-green-500 w-full my-3'></hr>
+            <ItemList artworks={list} onToggle={handleToggle} />
         </div>
     )
 }
+
+ItemList.propTypes = {
+    artworks: PropTypes.array,
+    onToggle: PropTypes.func
+}
+
+function ItemList({ artworks, onToggle }) {
+    return (
+      <ul>
+        {artworks.map(artwork => (
+          <li key={artwork.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={artwork.seen}
+                onChange={e => {
+                  onToggle(
+                    artwork.id,
+                    e.target.checked
+                  );
+                }}
+              />
+              {artwork.title}
+            </label>
+          </li>
+        ))}
+      </ul>
+    );
+  }
